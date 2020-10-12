e7a4215eb1f7296d5f7bb8501245a5c75ed2e729
Taylor Downs <downs.taylor@gmail.com>
2020-10-12 19:54:32 +0100

    update docs and deps

--- mix.exs
+++ mix.exs
@@ -4,12 +4,22 @@ defmodule Microservice.MixProject do
   def project do
     [
       app: :microservice,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      # Docs
+      name: "MyApp",
+      source_url: "https://github.com/openfn/microservice",
+      homepage_url: "https://www.openfn.org",
+      docs: [
+        # The main page in the docs
+        main: "Microservice",
+        # logo: "path/to/logo.png",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -32,10 +42,11 @@ defmodule Microservice.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.12"},
-      {:phoenix_pubsub, "~> 1.1"},
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
+      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix, "~> 1.4.12"},
+      {:plug_cowboy, "~> 2.4"},
       {:temp, "~> 0.4.7"}
     ]
   end
