bf901d487627d05d50fc0a78b88bd01a15c79af5
Taylor Downs <downs.taylor@gmail.com>
2020-11-05 12:30:27 +0000

    end to end test

--- test/fixtures/expression.js
+++ test/fixtures/expression.js
@@ -1,6 +1,6 @@
 // A test job to run with my microservice.
 alterState(state => {
-  // state.data.array.push(4);
+  state.data.array.push(4);
   state.newKey = true;
   console.log('Something in the logs.');
   return state;
